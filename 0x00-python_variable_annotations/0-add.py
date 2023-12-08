583a0a1a5cb829e0b9bb3dd3cf80f233e264c5ac
Franklinson <franklinhassey@gmail.com>
2023-12-08 23:02:49 +0000

    adding a type annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Annotation function that returns float"""
+
+def add(a: float, b: float) -> float:
+    """type annotated function, takes two floats
+    and returns a float of their sum
+    """
+    return float(a + b)
