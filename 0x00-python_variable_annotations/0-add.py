9d46a87464488780cec6b1d912a23f45541d6590
Franklinson <franklinhassey@gmail.com>
2023-12-08 23:05:44 +0000

    takes to strings and concat

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Annotation function that returns float"""
 
+
 def add(a: float, b: float) -> float:
     """type annotated function, takes two floats
     and returns a float of their sum
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""a type annonation functions return concat strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """takes two strings and returns them concated"""
+    return (str1 + str2)
