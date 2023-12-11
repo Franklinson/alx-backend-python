b6fe23bed11de16f37619845695b4c2db75fc942
Franklinson <franklinhassey@gmail.com>
2023-12-11 22:55:12 +0000

    python async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,11 +10,3 @@ async def wait_random(max_delay=10):
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
-
-
-async def main():
-    random_delay = await wait_random()
-    print(f"Random delay: {random_delay:.2f} seconds")
-
-if __name__ == "__main__":
-    asyncio.run(main())
