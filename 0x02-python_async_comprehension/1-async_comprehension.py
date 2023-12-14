41903a4b999195a5daa7a6ac6bd4ff93f622d268
Franklinson <franklinhassey@gmail.com>
2023-12-14 09:39:55 +0000

    will collect 10 random numbers using an async comprehensing over async_generator, then return the 10 random numbers

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-""" The coroutine will collect 10 random numbers
-using an async comprehensing over async_generator,
-then return the 10 random numbers. """
+"""Import async_generator from the previous task and then write a coroutine
+called async_comprehension that takes no arguments
+"""
 
 
 from typing import List
@@ -9,6 +9,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """ Using async comprehension to collect 10 random numbers """
-    random_numbers = [i async for i in async_generator()]
-    return random_numbers
+    """Using async comprehension to collect 10 random numbers"""
+    results = [i async for i in async_generator()]
+    return results
