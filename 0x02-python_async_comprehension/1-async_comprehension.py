c6a46efc62b91c3af699782f286c2dfc471a6c9e
Franklinson <franklinhassey@gmail.com>
2023-12-14 09:35:00 +0000

    will collect 10 random numbers using an async comprehensing over async_generator, then return the 10 random numbers

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,12 @@
 using an async comprehensing over async_generator,
 then return the 10 random numbers. """
 
+
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    # Using async comprehension to collect 10 random numbers
+    """ Using async comprehension to collect 10 random numbers """
     random_numbers = [value async for value in async_generator()]
     return random_numbers
